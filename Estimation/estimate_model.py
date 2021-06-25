51b08a25b2a42683f6cac4a60621c8282a5a7bbf
Youan Wang <wangyouan0629@hotmail.com>
2021-06-25 22:06:22 +0800

    update code

--- Estimation/estimate_model.py
+++ Estimation/estimate_model.py
@@ -90,11 +90,15 @@ def criterion(params, *args):
 
 if __name__ == '__main__':
     params_init_1 = np.array([-2.2067, 0.8349, 0.3594, 0.0449, 29.9661, 0.3816, 0.1829])
-    results1_1 = opt.minimize(criterion, params_init_1, args=(2,), method='L-BFGS-B',
-                              options={'eps': 1e-1, 'gtol': 1e-3},
-                              bounds=(
-                                  (-6.5, -0.5), (0.3, 0.9), (0.05, 0.6), (0.01, 0.2), (3, 30), (0.1, 0.7),
-                                  (0.01, 0.25)))
+    # results1_1 = opt.minimize(criterion, params_init_1, args=(2,), method='L-BFGS-B',
+    #                           options={'eps': 1e-1, 'gtol': 1e-3},
+    #                           bounds=(
+    #                               (-6.5, -0.5), (0.3, 0.9), (0.05, 0.6), (0.01, 0.2), (3, 30), (0.1, 0.7),
+    #                               (0.01, 0.25)))
+    results1_1 = opt.dual_annealing(criterion, args=(1,), x0=params_init_1,
+                                    bounds=(
+                                        (-3.5, -1.5), (0.8, 0.9), (0.3, 0.4), (0.01, 0.05), (25, 30), (0.1, 0.4),
+                                        (0.01, 0.25)))
     print(results1_1)
     # mu, rho, sigma, delta, gamma, theta, lambda_ = params_init_1
     # process_num: int = 2
